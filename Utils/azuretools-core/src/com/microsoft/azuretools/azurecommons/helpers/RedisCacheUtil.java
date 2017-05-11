78f838f70d9d32710f91a3baff3c1b8354df8b6c
sheche@microsoft.com <Szbn11dtsb`>
2017-05-11 13:14:34 +0800

    extract out the strings to static const

--- Utils/azuretools-core/src/com/microsoft/azuretools/azurecommons/helpers/RedisCacheUtil.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/azurecommons/helpers/RedisCacheUtil.java
@@ -31,48 +31,75 @@ import java.util.LinkedHashMap;
 
 public final class RedisCacheUtil {
 
+	// All kinds of Redis Cache Skus
+	private static final String C0_BASIC_SKU = "C0 Basic 250MB/Low network band/Shared infrastructure/256 Connections";
+	private static final String C1_BASIC_SKU = "C1 Basic 1GB/Low network band/Dedicated service/1,000 Connections";
+	private static final String C2_BASIC_SKU = "C2 Basic 2.5GB/Moderate network band/Dedicated infrastructure/2,000 Connections";
+	private static final String C3_BASIC_SKU = "C3 Basic 6GB/Moderate network band/Dedicated infrastructure/5,000 Connections";
+	private static final String C4_BASIC_SKU = "C4 Basic 13GB/Moderate network band/Dedicated infrastructure/10,000 Connections";
+	private static final String C5_BASIC_SKU = "C5 Basic 26GB/High network band/Dedicated infrastructure/15,000 Connections";
+	private static final String C6_BASIC_SKU = "C6 Basic 53GB/Highest network band/Dedicated infrastructure/20,000 Connections";
+	private static final String C0_STANDARD_SKU = "C0 Standard 250MB/Replication/Low network band/Shared infrastructure/256 Connections";
+	private static final String C1_STANDARD_SKU = "C1 Standard 1GB/Replication/Low network band/Dedicated service/1,000 Connections";
+	private static final String C2_STANDARD_SKU = "C2 Standard 2.5MB/Replication/Moderate network band/Dedicated service/2,000 Connections";
+	private static final String C3_STANDARD_SKU = "C3 Standard 6GB/Replication/Moderate network band/Dedicated service/5,000 Connections";
+	private static final String C4_STANDARD_SKU = "C4 Standard 13GB/Replication/Moderate network band/Dedicated service/10,000 Connections";
+	private static final String C5_STANDARD_SKU = "C5 Standard 26GB/Replication/High network band/Dedicated service/15,000 Connections";
+	private static final String C6_STANDARD_SKU = "C6 Standard 53GB/Replication/High network band/Dedicated service/20,000 Connections";
+	private static final String P1_PREMIUM_SKU = "P1 Premium 6GB/Replication/Moderate network band/All Standard features/Data Persist/Virtual Net/Cluster";
+	private static final String P2_PREMIUM_SKU = "P2 Premium 13GB/Replication/Moderate network band/All Standard features/Data Persist/Virtual Net/Cluster";
+	private static final String P3_PREMIUM_SKU = "P3 Premium 26GB/Replication/High network band/All Standard features/Data Persist/Virtual Net/Cluster";
+	private static final String P4_PREMIUM_SKU = "P4 Premium 53GB/Replication/Highest network band/All Standard features/Data Persist/Virtual Net/Cluster";
+
+	//Alert Messages
+	private static final String REQUIRE_SUBSCRIPTION = "Select one subscription for Redis cache creation.";
+	private static final String INVALID_REDIS_CACHE_NAME = "Invalid Redis Cache name. The name can only contain letters, numbers and hyphens. The first and last characters must each be a letter or a number. Consecutive hyphens are not allowed.";
+	private static final String REQUIRE_LOCATION = "Location cannot be null or empty.";
+	private static final String REQUIRE_RESOURCE_GROUP = "Resource group cannot be null or empty.";
+	private static final String REQUIRE_PRICE_TIER = "Pricing tier cannot be null or empty.";
+
     public static LinkedHashMap<String, String> initSkus() {
     	LinkedHashMap<String, String> skus = new LinkedHashMap<String, String>();
-        skus.put("C0 Basic 250MB/Low network band/Shared infrastructure/256 Connections", "BASIC0");
-        skus.put("C1 Basic 1GB/Low network band/Dedicated service/1,000 Connections", "BASIC1");
-        skus.put("C2 Basic 2.5GB/Moderate network band/Dedicated infrastructure/2,000 Connections", "BASIC2");
-        skus.put("C3 Basic 6GB/Moderate network band/Dedicated infrastructure/5,000 Connections", "BASIC3");
-        skus.put("C4 Basic 13GB/Moderate network band/Dedicated infrastructure/10,000 Connections", "BASIC4");
-        skus.put("C5 Basic 26GB/High network band/Dedicated infrastructure/15,000 Connections", "BASIC5");
-        skus.put("C6 Basic 53GB/Highest network band/Dedicated infrastructure/20,000 Connections", "BASIC6");
-        skus.put("C0 Standard 250MB/Replication/Low network band/Shared infrastructure/256 Connections", "STD0");
-        skus.put("C1 Standard 1GB/Replication/Low network band/Dedicated service/1,000 Connections", "STD1");
-        skus.put("C2 Standard 2.5MB/Replication/Moderate network band/Dedicated service/2,000 Connections", "STD2");
-        skus.put("C3 Standard 6GB/Replication/Moderate network band/Dedicated service/5,000 Connections", "STD3");
-        skus.put("C4 Standard 13GB/Replication/Moderate network band/Dedicated service/10,000 Connections", "STD4");
-        skus.put("C5 Standard 26GB/Replication/High network band/Dedicated service/15,000 Connections", "STD5");
-        skus.put("C6 Standard 53GB/Replication/High network band/Dedicated service/20,000 Connections", "STD6");
-        skus.put("P1 Premium 6GB/Replication/Moderate network band/All Standard features/Data Persist/Virtual Net/Cluster", "PREMIUM1");
-        skus.put("P2 Premium 13GB/Replication/Moderate network band/All Standard features/Data Persist/Virtual Net/Cluster", "PREMIUM2");
-        skus.put("P3 Premium 26GB/Replication/High network band/All Standard features/Data Persist/Virtual Net/Cluster", "PREMIUM3");
-        skus.put("P4 Premium 53GB/Replication/Highest network band/All Standard features/Data Persist/Virtual Net/Cluster", "PREMIUM4");
+        skus.put(C0_BASIC_SKU, "BASIC0");
+        skus.put(C1_BASIC_SKU, "BASIC1");
+        skus.put(C2_BASIC_SKU, "BASIC2");
+        skus.put(C3_BASIC_SKU, "BASIC3");
+        skus.put(C4_BASIC_SKU, "BASIC4");
+        skus.put(C5_BASIC_SKU, "BASIC5");
+        skus.put(C6_BASIC_SKU, "BASIC6");
+        skus.put(C0_STANDARD_SKU, "STD0");
+        skus.put(C1_STANDARD_SKU, "STD1");
+        skus.put(C2_STANDARD_SKU, "STD2");
+        skus.put(C3_STANDARD_SKU, "STD3");
+        skus.put(C4_STANDARD_SKU, "STD4");
+        skus.put(C5_STANDARD_SKU, "STD5");
+        skus.put(C6_STANDARD_SKU, "STD6");
+        skus.put(P1_PREMIUM_SKU, "PREMIUM1");
+        skus.put(P2_PREMIUM_SKU, "PREMIUM2");
+        skus.put(P3_PREMIUM_SKU, "PREMIUM3");
+        skus.put(P4_PREMIUM_SKU, "PREMIUM4");
         return skus;
     }
 
     public static boolean doValidate(AzureManager azureManager, SubscriptionDetail currentSub, String dnsNameValue, String selectedRegionValue, String selectedResGrpValue, String selectedPriceTierValue) {
     	if (currentSub == null) {
-    		JOptionPane.showMessageDialog(null, "Select one subscription for Redis cache creation", "Alert", JOptionPane.ERROR_MESSAGE, null);
+    		JOptionPane.showMessageDialog(null, REQUIRE_SUBSCRIPTION, "Alert", JOptionPane.ERROR_MESSAGE, null);
     		return false;
     	}
     	if (dnsNameValue == null || dnsNameValue.isEmpty() || !dnsNameValue.matches("^[A-Za-z0-9]+(-[A-Za-z0-9]+)*$")) {
-    		JOptionPane.showMessageDialog(null, "Invalid Redis Cache name. The name can only contain letters, numbers and hyphens. The first and last characters must each be a letter or a number. Consecutive hyphens are not allowed.", "Alert", JOptionPane.ERROR_MESSAGE, null);
+    		JOptionPane.showMessageDialog(null, INVALID_REDIS_CACHE_NAME, "Alert", JOptionPane.ERROR_MESSAGE, null);
     		return false;
     	}
     	if (selectedRegionValue == null || selectedRegionValue.isEmpty()) {
-    		JOptionPane.showMessageDialog(null, "Location cannot be null or empty", "Alert", JOptionPane.ERROR_MESSAGE, null);
+    		JOptionPane.showMessageDialog(null, REQUIRE_LOCATION, "Alert", JOptionPane.ERROR_MESSAGE, null);
     		return false;
     	}
     	if (selectedResGrpValue == null || selectedResGrpValue.isEmpty()) {
-    		JOptionPane.showMessageDialog(null, "Resource group cannot be null or empty", "Alert", JOptionPane.ERROR_MESSAGE, null);
+    		JOptionPane.showMessageDialog(null, REQUIRE_RESOURCE_GROUP, "Alert", JOptionPane.ERROR_MESSAGE, null);
     		return false;
     	}
     	if (selectedPriceTierValue == null || selectedPriceTierValue.isEmpty()) {
-    		JOptionPane.showMessageDialog(null, "Pricing tier cannot be null or empty", "Alert", JOptionPane.ERROR_MESSAGE, null);
+    		JOptionPane.showMessageDialog(null, REQUIRE_PRICE_TIER, "Alert", JOptionPane.ERROR_MESSAGE, null);
     		return false;
     	}
     	try {
