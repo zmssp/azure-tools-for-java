c2743f4aa3603f6594b8118044a3a8f4f518cf28
look4pku <djlan@microsoft.com>
2017-05-11 12:40:39 +0800

    Add IntelliJ extension point for Spark remote debug run configuration

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/hdinsight/spark/run/configuration/RemoteDebugRunConfigurationType.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) Microsoft Corporation
+ * <p/>
+ * All rights reserved.
+ * <p/>
+ * MIT License
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.microsoft.azure.hdinsight.spark.run.configuration;
+
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.openapi.util.IconLoader;
+
+import com.microsoft.azure.hdinsight.common.CommonConst;
+import com.microsoft.intellij.util.PluginUtil;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class RemoteDebugRunConfigurationType implements ConfigurationType {
+
+    private static final String DISPLAY_NAME = "Spark Remote Debug";
+    private static final String ID = "SparkRemoteDebug_Run_Configuration";
+    private static final String DESCRIPTION = "Spark Remote Debug Run Configuration Type";
+
+    @Override
+    public String getDisplayName() {
+        return DISPLAY_NAME;
+    }
+
+    @Override
+    public String getConfigurationTypeDescription() {
+        return DESCRIPTION;
+    }
+
+    @Override
+    public Icon getIcon() {
+        return PluginUtil.getIcon(CommonConst.OpenSparkUIIconPath);
+    }
+
+    @NotNull
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public ConfigurationFactory[] getConfigurationFactories() {
+        return new ConfigurationFactory[0];
+    }
+}
