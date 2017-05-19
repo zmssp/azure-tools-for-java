d020938ec2026bde9e6579c44a55940d95fa0cbe
Jinbo Wang <jinbwan@microsoft.com>
2017-05-19 14:21:00 +0800

    Make travis-ci and jenkins-ci share the same build script

--- tools/IntellijVersionHelper.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git checkout -- ../PluginsAndFeatures/azure-toolkit-for-intellij/resources/META-INF/plugin.xml
-javac IntellijVersionHelper.java
-java IntellijVersionHelper %1
-exit %ERRORLEVEL%
