ed0006ea720985590d15527939e1e28f1e30806d
Jinbo Wang <jinbwan@microsoft.com>
2017-05-22 13:44:49 +0800

    Fix BuildDevint script to modify Intellij plugin version

--- tools/XMLHelper.java
+++ tools/XMLHelper.java
@@ -18,39 +18,54 @@ import org.w3c.dom.Node;
 
 // Run the program via the cli command "java VersionHelper 2016.3"
 // This program will append intellij version to the plugin version.
-public class IntellijVersionHelper {
+public class XMLHelper {
 
 	public static void main(String argv[]) {
-		if (argv.length < 1) {
-			System.out.println("[Failed] Please specify the target intellij version for the plugin.");
+		if (argv.length < 4) {
+			System.out.println("[Failed] Please specify the arguments $XML_FILE $XPATH $NEW_VALUE $JOIN_OR_REPLACE.");
 			System.exit(1);
 		}
 
+		String FILEPATH = argv[0];
+		String XPATH = argv[1];
+		String NEW_VALUE = argv[2];
+		String CHANGETYPE = argv[3];
+		boolean DISPLAY_LOG = false;
+		if (argv.length > 4 && argv[4].toLowerCase().equals("true")) {
+			DISPLAY_LOG = true;
+		}
+
 		try {
+			System.out.println("Starting to modify XML " + FILEPATH);
+			
 			// Read the content from xml file
-			String filepath = "../PluginsAndFeatures/azure-toolkit-for-intellij/resources/META-INF/plugin.xml";
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			Document doc = docBuilder.parse(filepath);
+			Document doc = docBuilder.parse(FILEPATH);
 
 			XPathFactory xpathFactory = XPathFactory.newInstance();
 			XPath xpath = xpathFactory.newXPath();
-			XPathExpression expression =  xpath.compile("/idea-plugin/version/text()");
-			Node versionNode = (Node) expression.evaluate(doc, XPathConstants.NODE);
-			String oldVersion = versionNode.getNodeValue();
-			String newVersion = oldVersion + "-" + argv[0];
-			System.out.println("The old version is " + oldVersion);
-			System.out.println("The new version is " + newVersion);
-			versionNode.setNodeValue(newVersion);
+			XPathExpression expression =  xpath.compile(XPATH);
+			Node targetNode = (Node) expression.evaluate(doc, XPathConstants.NODE);
+			String oldValue = targetNode.getNodeValue();
+			String newValue = "";
+			if (CHANGETYPE.equals("JOIN")) {
+				newValue = oldValue + NEW_VALUE;
+			}
+			if (DISPLAY_LOG) {
+				System.out.println("The old value is " + oldValue);
+				System.out.println("The new value is " + newValue);
+			}
+			targetNode.setNodeValue(newValue);
 
 			// Write the content into xml file
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
 			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File(filepath));
+			StreamResult result = new StreamResult(new File(FILEPATH));
 			transformer.transform(source, result);
 
-			System.out.println("Done");
+			System.out.println("Modify XML Finished.");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
