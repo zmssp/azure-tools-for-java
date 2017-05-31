8716658487f8b2c5bc63a15a62a9f8827e8f6e64
ltian <ltian@microsoft.com>
2017-05-31 12:28:51 +0800

    add dependencies to hdinsight

--- Utils/hdinsight-node-common/src/com/microsoft/azure/hdinsight/common/task/YarnHistoryTask.java
+++ Utils/hdinsight-node-common/src/com/microsoft/azure/hdinsight/common/task/YarnHistoryTask.java
@@ -29,16 +29,10 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.microsoft.azure.hdinsight.sdk.cluster.IClusterDetail;
 import com.microsoft.azure.hdinsight.sdk.common.HDIException;
 import com.microsoft.azuretools.azurecommons.helpers.NotNull;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
 public class YarnHistoryTask extends Task<String> {
 
