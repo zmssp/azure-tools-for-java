8968824b9816bd934935e044e5b0a90070bea522
sheche <sheche@microsoft.com>
2017-06-02 17:06:07 +0800

    change tab to space

--- Utils/azuretools-core/src/com/microsoft/azuretools/azurecommons/azuremodel/AzureModel.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/azurecommons/azuremodel/AzureModel.java
@@ -11,28 +11,28 @@ import com.microsoft.azuretools.authmanage.SubscriptionManager;
 import com.microsoft.azuretools.sdkmanage.AzureManager;
 
 public class AzureModel {
-	public static HashMap<String, RedisCaches> getRedisCaches() throws IOException {
-		HashMap<String, RedisCaches> redisCacheMaps = new HashMap<String, RedisCaches>();
-		AzureManager azureManager = AuthMethodManager.getInstance().getAzureManager();
+    public static HashMap<String, RedisCaches> getRedisCaches() throws IOException {
+        HashMap<String, RedisCaches> redisCacheMaps = new HashMap<String, RedisCaches>();
+        AzureManager azureManager = AuthMethodManager.getInstance().getAzureManager();
         if (azureManager == null) {
             return redisCacheMaps;
         }
         SubscriptionManager subscriptionManager = azureManager.getSubscriptionManager();
         Set<String> sidList = subscriptionManager.getAccountSidList();
         for (String sid : sidList) {
-        	Azure azure = azureManager.getAzure(sid);
+            Azure azure = azureManager.getAzure(sid);
             redisCacheMaps.put(sid, azure.redisCaches());
         }
         return redisCacheMaps;
-	}
-	
-	public static void deleteRedisCache(String sid, String id) throws IOException {
-		AzureManager azureManager = AuthMethodManager.getInstance().getAzureManager();
+    }
+    
+    public static void deleteRedisCache(String sid, String id) throws IOException {
+        AzureManager azureManager = AuthMethodManager.getInstance().getAzureManager();
         if (azureManager == null) {
             return;
         }
         Azure azure = azureManager.getAzure(sid);
         RedisCaches redisCaches = azure.redisCaches();
         redisCaches.deleteById(id);
-	}
+    }
 }
