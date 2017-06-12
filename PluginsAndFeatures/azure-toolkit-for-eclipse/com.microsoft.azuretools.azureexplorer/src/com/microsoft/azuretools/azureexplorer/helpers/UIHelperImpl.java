d407ab3d8368c0dec7d513d6e840948ffb7122c5
sheche <sheche@microsoft.com>
2017-06-12 11:44:27 +0800

    eclipse can show multiple property view for different redis

--- PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.azureexplorer/src/com/microsoft/azuretools/azureexplorer/helpers/UIHelperImpl.java
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.azureexplorer/src/com/microsoft/azuretools/azureexplorer/helpers/UIHelperImpl.java
@@ -270,7 +270,7 @@ public class UIHelperImpl implements UIHelper {
             if (page == null) {
                 return;
             }
-            RedisPropertyView view = (RedisPropertyView) page.showView(RedisPropertyView.ID);
+            RedisPropertyView view = (RedisPropertyView) page.showView(RedisPropertyView.ID, node.getResourceId(), IWorkbenchPage.VIEW_ACTIVATE);
             Display.getDefault().asyncExec(new Runnable() {
                 @Override
                 public void run() {
