d49f96516a043103dcdd1930cafdaac069651c81
ziyli <ziyli@microsoft.com>
2017-07-04 11:27:50 +0800

    update module file path to lower case when using sbt to generated the project

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/hdinsight/projects/HDInsightModuleBuilder.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/hdinsight/projects/HDInsightModuleBuilder.java
@@ -23,11 +23,15 @@ package com.microsoft.azure.hdinsight.projects;
 
 import com.intellij.ide.util.projectWizard.*;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.module.ModifiableModuleModel;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.module.ModuleWithNameAlreadyExists;
+import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.packaging.artifacts.ArtifactManager;
 import com.intellij.packaging.elements.CompositePackagingElement;
 import com.intellij.packaging.elements.PackagingElementFactory;
@@ -37,10 +41,13 @@ import com.microsoft.azure.hdinsight.common.CommonConst;
 import com.microsoft.azure.hdinsight.projects.ui.HDInsightProjectTypeStep;
 import com.microsoft.azuretools.telemetry.AppInsightsClient;
 import com.microsoft.intellij.hdinsight.messages.HDInsightBundle;
+import org.jdom.JDOMException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -101,6 +108,15 @@ public class HDInsightModuleBuilder extends JavaModuleBuilder implements ModuleB
         }
     }
 
+    @NotNull
+    @Override
+    public Module createModule(@NotNull ModifiableModuleModel moduleModel) throws InvalidDataException, IOException, ModuleWithNameAlreadyExists, JDOMException, ConfigurationException {
+        if (this.selectedExternalSystem == HDInsightExternalSystem.SBT) {
+            this.updateModulePath();
+        }
+        return super.createModule(moduleModel);
+    }
+
     @Nullable
     @Override
     public ModuleWizardStep getCustomOptionsStep(WizardContext context, Disposable parentDisposable) {
@@ -148,6 +164,12 @@ public class HDInsightModuleBuilder extends JavaModuleBuilder implements ModuleB
 
     public HDInsightExternalSystem getSelectedExternalSystem() {
         return this.selectedExternalSystem;
+    }
+
+    private void updateModulePath() {
+        File file = new File(this.getModuleFilePath());
+        String path = file.getParent() + "/" + file.getName().toLowerCase();
+        this.setModuleFilePath(path);
     }
 
     private void initTemplates() {
