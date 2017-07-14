c597c0f7f1ab45380fb97bc559ce2eb78f226f4c
yanzh <yanzh@microsoft.com>
2017-07-14 10:51:51 +0800

    add copyright

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/container/ui/PublishWizardModel.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/container/ui/PublishWizardModel.java
@@ -1,12 +1,31 @@
+/**
+ * Copyright (c) Microsoft Corporation
+ * <p/>
+ * All rights reserved.
+ * <p/>
+ * MIT License
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.microsoft.intellij.container.ui;
 
 import com.intellij.ui.wizard.WizardModel;
 import com.intellij.ui.wizard.WizardNavigationState;
 import com.intellij.ui.wizard.WizardStep;
 
-/**
- * Created by yanzh on 7/11/2017.
- */
 public class PublishWizardModel extends WizardModel{
     private static final String TEXT_WINDOW_TITLE = "Publish to Web App on Linux";
     private boolean imagePushed = false;
