7f0a89d3838b0a95a59abf9cb5e0f7c0c2033a17
sheche <sheche@microsoft.com>
2017-09-14 14:33:17 +0800

    modify according to comments

--- Utils/azuretools-core/src/com/microsoft/azuretools/core/mvp/model/container/ContainerExplorerMvpModel.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/core/mvp/model/container/ContainerExplorerMvpModel.java
@@ -31,14 +31,17 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import java.util.Map;
+
 public class ContainerExplorerMvpModel {
 
-    private static final OkHttpClient sharedClient = new OkHttpClient();
-    private static final String URL_PREFIX = "https://";
-    private static final String REPOSITORY_PATH = "/v2/_catalog";
-    private static final String TAG_PATH = "/v2/%s/tags/list";
+    private static final String URL_PREFIX = "https";
+    private static final String REPOSITORY_PATH = "v2/_catalog";
+    private static final String TAG_PATH = "v2/%s/tags/list";
     private static final String HEADER_AUTH = "Authorization";
-    private static final String INVALID_URL = "Invalid URL: ";
+    private static final String INVALID_URL = "The request URL is NULL.";
+
+    private final OkHttpClient sharedClient = new OkHttpClient();
 
     private ContainerExplorerMvpModel() {
     }
@@ -55,31 +58,44 @@ public class ContainerExplorerMvpModel {
      * list repositories under the given private registry.
      */
     public String listRepositories(@NotNull String serverUrl, @NotNull String username, @NotNull String password,
-                                   @Nullable String queryString) throws Exception {
+                                   @Nullable Map<String, String> query) throws Exception {
         OkHttpClient client = createRestClient(username, password);
-        final String host = URL_PREFIX + serverUrl;
-        String path = REPOSITORY_PATH + (queryString == null ? "" : queryString);
-        return getResponse(client, host + path);
+        HttpUrl.Builder urlBuilder = new HttpUrl.Builder()
+                .scheme(URL_PREFIX)
+                .host(serverUrl)
+                .addPathSegment(REPOSITORY_PATH);
+        if (query != null) {
+            for (String key : query.keySet()) {
+                urlBuilder.addQueryParameter(key, query.get(key));
+            }
+        }
+        return getResponse(client, urlBuilder.build());
     }
 
     /**
      * list repositories under the given repository.
      */
     public String listTags(@NotNull String serverUrl, @NotNull String username, @NotNull String password,
-                                 @NotNull String repo, @Nullable String queryString) throws Exception {
+                                 @NotNull String repo, @Nullable Map<String, String> query) throws Exception {
         OkHttpClient client = createRestClient(username, password);
-        final String host = URL_PREFIX + serverUrl;
-        String path = String.format(TAG_PATH, repo) + (queryString == null ? "" : queryString);
-        return getResponse(client, host + path);
+        HttpUrl.Builder urlBuilder = new HttpUrl.Builder()
+                .scheme(URL_PREFIX)
+                .host(serverUrl)
+                .addPathSegment(String.format(TAG_PATH, repo));
+        if (query != null) {
+            for (String key : query.keySet()) {
+                urlBuilder.addQueryParameter(key, query.get(key));
+            }
+        }
+        return getResponse(client, urlBuilder.build());
     }
 
     @NotNull
-    private String getResponse(@NotNull OkHttpClient client, @NotNull String url) throws Exception {
-        HttpUrl route = HttpUrl.parse(url);
-        if (route == null) {
-            throw new Exception(INVALID_URL + url);
+    private String getResponse(@NotNull OkHttpClient client, HttpUrl url) throws Exception {
+        if (url == null) {
+            throw new NullPointerException(INVALID_URL);
         }
-        Request request = new Request.Builder().url(route).get().build();
+        Request request = new Request.Builder().url(url).get().build();
         try (Response response = client.newCall(request).execute()) {
             if (response.isSuccessful()) {
                 return response.body().string();
