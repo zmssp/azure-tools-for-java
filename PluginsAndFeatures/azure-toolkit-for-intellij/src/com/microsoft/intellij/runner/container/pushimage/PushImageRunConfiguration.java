b276a6aa91b49589960ee3683cd2999171b27717
sheche <sheche@microsoft.com>
2017-09-30 11:03:32 +0800

    modify accoding to comments

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/runner/container/pushimage/PushImageRunConfiguration.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/runner/container/pushimage/PushImageRunConfiguration.java
@@ -58,6 +58,13 @@ public class PushImageRunConfiguration extends RunConfigurationBase {
     private static final String INVALID_ARTIFACT_FILE = "The artifact name %s is invalid. "
             + "An artifact name may contain only the ASCII letters 'a' through 'z' (case-insensitive), "
             + "and the digits '0' through '9', '.', '-' and '_'.";
+    private static final String CANNOT_END_WITH_COLON = "Image and tag name cannot end with ':'";
+    private static final String REPO_LENGTH_INVALID = "The length of repository name must be at least one character "
+            + "and less than 256 characters";
+    private static final String CANNOT_END_WITH_SLASH = "The repository name should not end with '/'";
+    private static final String REPO_COMPONENT_INVALID = "Invalid repository component: %s, should follow: %s";
+    private static final String TAG_LENGTH_INVALID = "The length of tag name must be no more than 128 characters";
+    private static final String TAG_INVALID = "Invalid tag: %s, should follow: %s";
     private static final String MISSING_MODEL = "Configuration data model not initialized.";
     private static final String ARTIFACT_NAME_REGEX = "^[.A-Za-z0-9_-]+\\.(war|jar)$";
     private static final String DOMAIN_NAME_REGEX = "^([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,}$";
@@ -124,34 +131,39 @@ public class PushImageRunConfiguration extends RunConfigurationBase {
         if (Utils.isEmptyString(setting.getPassword())) {
             throw new ConfigurationException(MISSING_PASSWORD);
         }
-        if (Utils.isEmptyString(setting.getImageNameWithTag())) {
+        String imageTag = setting.getImageNameWithTag();
+        if (Utils.isEmptyString(imageTag)) {
             throw new ConfigurationException(MISSING_IMAGE_WITH_TAG);
         }
-        String serverUrl = setting.getServerUrl() + "/";
-        String imageTag = "";
-        int index = setting.getImageNameWithTag().indexOf(serverUrl);
-        if (index >= 0) {
-            imageTag = setting.getImageNameWithTag().substring(index + serverUrl.length());
+        if (imageTag.endsWith(":")) {
+            throw new ConfigurationException(CANNOT_END_WITH_COLON);
         }
-        // check image and tag length and format
         final String[] repoAndTag = imageTag.split(":");
-        if (imageTag.endsWith(":") || repoAndTag.length != 2 || repoAndTag[0].length() < 1
-                || repoAndTag[0].length() + serverUrl.length() > REPO_LENGTH || repoAndTag[1].length() < 1
-                || repoAndTag[1].length() > TAG_LENGTH) {
-            throw new ConfigurationException(INVALID_IMAGE_WITH_TAG);
+
+        // check repository first
+        if (repoAndTag[0].length() < 1 || repoAndTag[0].length() > REPO_LENGTH) {
+            throw new ConfigurationException(REPO_LENGTH_INVALID);
         }
-        // check repo
         if (repoAndTag[0].endsWith("/")) {
-            throw new ConfigurationException(INVALID_IMAGE_WITH_TAG);
+            throw new ConfigurationException(CANNOT_END_WITH_SLASH);
         }
         final String[] repoComponents = repoAndTag[0].split("/");
         for (String component : repoComponents) {
             if (!component.matches(REPO_COMPONENTS_REGEX)) {
-                throw new ConfigurationException(INVALID_IMAGE_WITH_TAG);
+                throw new ConfigurationException(String.format(REPO_COMPONENT_INVALID, component,
+                        REPO_COMPONENTS_REGEX));
             }
         }
-        // check tag
-        if (!repoAndTag[1].matches(TAG_REGEX)) {
+        // check when contains tag
+        if (repoAndTag.length == 2) {
+            if (repoAndTag[1].length() > TAG_LENGTH) {
+                throw new ConfigurationException(TAG_LENGTH_INVALID);
+            }
+            if (!repoAndTag[1].matches(TAG_REGEX)) {
+                throw new ConfigurationException(String.format(TAG_INVALID, repoAndTag[1], TAG_REGEX));
+            }
+        }
+        if (repoAndTag.length > 2) {
             throw new ConfigurationException(INVALID_IMAGE_WITH_TAG);
         }
         // target package
