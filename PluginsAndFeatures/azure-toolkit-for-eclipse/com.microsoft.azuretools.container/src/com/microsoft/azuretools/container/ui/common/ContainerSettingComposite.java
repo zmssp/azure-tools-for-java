3a3691f62a8c0a1645970d7cc59b2cacdd876c6f
sheche@microsoft.com <Szbn11dtsb`>
2017-10-11 16:23:29 +0800

    init acr common ui

---
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.container/src/com/microsoft/azuretools/container/ui/common/ContainerSettingComposite.java
@@ -0,0 +1,222 @@
+package com.microsoft.azuretools.container.ui.common;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+
+import com.microsoft.azure.management.containerregistry.Registry;
+import com.microsoft.azuretools.azurecommons.helpers.NotNull;
+import com.microsoft.azuretools.container.Constant;
+import com.microsoft.azuretools.core.mvp.model.webapp.PrivateRegistryImageSetting;
+import com.microsoft.tooling.msservices.serviceexplorer.azure.container.ContainerSettingPresenter;
+import com.microsoft.tooling.msservices.serviceexplorer.azure.container.ContainerSettingView;
+
+public final class ContainerSettingComposite extends Composite implements ContainerSettingView {
+
+    private final ContainerSettingPresenter<ContainerSettingComposite> presenter;
+    private final String projectBasePath;
+    private List<Registry> registryCache;
+
+    private static final String LABEL_STARTUP_FILE = "Startup File";
+    private static final String LABEL_TAG_PREFIX = "<Server URL>/";
+    private static final String LABEL_IMAGE_AND_TAG = "Image and Tag";
+    private static final String LABEL_PASSWORD = "Password";
+    private static final String LABEL_USER_NAME = "User Name";
+    private static final String LABEL_SERVER_URL = "Server URL";
+    private static final String LABEL_CONTAINER_REGISTRY = "Container Registry";
+    private static final String SELECT_REGISTRY = "<Select Container Registry>";
+    private static final String LOADING = "<Loading...>";
+
+    private Text txtServerUrl;
+    private Text txtUserName;
+    private Text txtPassword;
+    private Text txtImageTag;
+    private Text txtStartupFile;
+    private Label lblStartupFile;
+    private Combo cbContainerRegistry;
+
+    /**
+     * Create the composite.
+     * @param parent
+     * @param style
+     */
+    public ContainerSettingComposite(Composite parent, int style, String basePath) {
+        super(parent, style);
+
+        presenter = new ContainerSettingPresenter<>();
+        presenter.onAttachView(this);
+        projectBasePath = basePath;
+        registryCache = new ArrayList<>();
+
+        setLayout(new GridLayout(3, false));
+
+        Label lblContainerRegistry = new Label(this, SWT.NONE);
+        lblContainerRegistry.setText(LABEL_CONTAINER_REGISTRY);
+
+        cbContainerRegistry = new Combo(this, SWT.NONE);
+        cbContainerRegistry.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+
+        Label lblServerUrl = new Label(this, SWT.NONE);
+        lblServerUrl.setText(LABEL_SERVER_URL);
+
+        txtServerUrl = new Text(this, SWT.BORDER);
+        txtServerUrl.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false, 2, 1));
+
+        Label lblUserName = new Label(this, SWT.NONE);
+        lblUserName.setText(LABEL_USER_NAME);
+
+        txtUserName = new Text(this, SWT.BORDER);
+        txtUserName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+
+        Label lblPassword = new Label(this, SWT.NONE);
+        lblPassword.setText(LABEL_PASSWORD);
+
+        txtPassword = new Text(this, SWT.BORDER | SWT.PASSWORD);
+        txtPassword.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+
+        Label lblImageAndTag = new Label(this, SWT.NONE);
+        lblImageAndTag.setText(LABEL_IMAGE_AND_TAG);
+
+        Label lblTagPrefix = new Label(this, SWT.NONE);
+        lblTagPrefix.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+        lblTagPrefix.setText(LABEL_TAG_PREFIX);
+
+        txtImageTag = new Text(this, SWT.BORDER);
+        txtImageTag.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+        lblStartupFile = new Label(this, SWT.NONE);
+        lblStartupFile.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+        lblStartupFile.setText(LABEL_STARTUP_FILE);
+
+        txtStartupFile = new Text(this, SWT.BORDER);
+        txtStartupFile.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+
+        cbContainerRegistry.addListener(SWT.Selection, new Listener() {
+            @Override
+            public void handleEvent(Event event) {
+                int index = cbContainerRegistry.getSelectionIndex();
+                String selectedItem = cbContainerRegistry.getItem(index);
+                if (selectedItem != null && (selectedItem.equals(LOADING) || selectedItem.equals(SELECT_REGISTRY))) {
+                    enableWidgets();
+                    return;
+                }
+                if (registryCache != null || index >= 1 || index < registryCache.size()) {
+                    disableWidgets();
+                    Registry registry = registryCache.get(index);
+                    presenter.onGetRegistryCredential(registry);
+                }
+            }
+        });
+
+        txtServerUrl.addListener(SWT.Modify, new Listener() {
+            @Override
+            public void handleEvent(Event arg0) {
+                lblTagPrefix.setText(txtServerUrl.getText() + "/");
+                adjustParentSize();
+            }
+        });
+    }
+
+    @Override
+    protected void checkSubclass() {
+        // Disable the check that prevents subclassing of SWT components
+    }
+
+    @Override
+    public void disposeEditor() {
+        presenter.onDetachView();
+    }
+
+    @Override
+    public void fillCredential(PrivateRegistryImageSetting setting) {
+        txtServerUrl.setText(setting.getServerUrl());
+        txtUserName.setText(setting.getUsername());
+        txtPassword.setText(setting.getPassword());
+        txtImageTag.setText(setting.getImageNameWithTag());
+        txtImageTag.setFocus();
+    }
+
+    @Override
+    public void listRegistries(@NotNull List<Registry> registryList) {
+        cbContainerRegistry.removeAll();
+        registryCache.clear();
+        cbContainerRegistry.add(SELECT_REGISTRY);
+        registryCache.add(null);
+        for (Registry registry : registryList) {
+            cbContainerRegistry.add(registry.name());
+        }
+        cbContainerRegistry.select(0);
+        registryCache.addAll(registryList);
+        adjustParentSize();
+    }
+
+    @Override
+    public void onListRegistries() {
+        cbContainerRegistry.removeAll();
+        cbContainerRegistry.add(LOADING);
+        cbContainerRegistry.select(0);
+        presenter.onListRegistries();
+    }
+
+    @Override
+    public void setStartupFileVisible(boolean visible) {
+        lblStartupFile.setVisible(visible);
+        txtStartupFile.setVisible(visible);
+        ((GridData) lblStartupFile.getLayoutData()).exclude = !visible;
+        ((GridData) txtStartupFile.getLayoutData()).exclude = !visible;
+        this.getParent().pack();
+    }
+
+    public String getServerUrl() {
+        return txtServerUrl.getText();
+    }
+
+    public String getUserName() {
+        return txtUserName.getText();
+    }
+
+    public String getPassword() {
+        return txtPassword.getText();
+    }
+
+    public String getImageTag() {
+        return txtImageTag.getText();
+    }
+
+    public String getDockerPath() {
+        return Paths.get(projectBasePath, Constant.DOCKERFILE_FOLDER, Constant.DOCKERFILE_NAME).toString();
+    }
+
+    public String getStartupFile() {
+        return txtStartupFile.getText();
+    }
+
+    private void disableWidgets() {
+        txtServerUrl.setEnabled(false);
+        txtUserName.setEnabled(false);
+        txtPassword.setEnabled(false);
+    }
+
+    private void enableWidgets() {
+        txtServerUrl.setEnabled(true);
+        txtUserName.setEnabled(true);
+        txtPassword.setEnabled(true);
+    }
+
+    private void adjustParentSize() {
+        this.getParent().layout(true, true);
+        final Point newSize = this.getParent().getShell().computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
+        this.getParent().getShell().setSize(newSize);
+    }
+}
