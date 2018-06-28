f9285a7efdc98535cba677dd9e468eaea3619ed3
Wei Zhang <wezhang@outlook.com>
2018-06-28 03:38:02 -0700

    Introduce ReportableError and GithubIssue classes

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/common/GithubIssue.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.microsoft.intellij.common
+
+import com.microsoft.tooling.msservices.components.DefaultLoader
+import org.apache.http.client.utils.URLEncodedUtils
+import org.apache.http.message.BasicNameValuePair
+import java.net.URI
+import java.nio.charset.StandardCharsets
+
+class GithubIssue(shortMessage: String, detailMessage: String) : ReportableError(shortMessage, detailMessage) {
+    private val labels = mutableSetOf("IntelliJ", "bug")
+
+    private val pluginRepo: URI
+        get() {
+            val url = if (plugin.url.endsWith("/")) plugin.url else plugin.url + "/"
+
+            return URI.create(url)
+        }
+
+    private fun getRequestUrl(): String {
+        return pluginRepo.resolve("issues/new?" + URLEncodedUtils.format(listOf(
+                BasicNameValuePair("title", getTitle()),
+                BasicNameValuePair("labels", labels.joinToString(",")),
+                BasicNameValuePair("body", getBody())
+        ), StandardCharsets.UTF_8)).toString()
+    }
+
+    override fun report() {
+        DefaultLoader.getIdeHelper().openLinkInBrowser(getRequestUrl())
+    }
+
+    fun withLabel(label: String): GithubIssue {
+        labels.add(label)
+
+        return this
+    }
+}
---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/common/ReportableError.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.microsoft.intellij.common
+
+import com.intellij.ide.plugins.PluginManager
+import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.extensions.PluginId
+
+abstract class ReportableError(private val shortMessage: String, private val detailMessage: String) {
+    val plugin = PluginManager.getPlugin(PluginId.getId("com.microsoft.tooling.msservices.intellij.azure"))!!
+    private val appInfo = ApplicationInfo.getInstance()
+
+    private val platformInfo = mutableMapOf<String, String>(
+            "IntelliJ build version" to "${appInfo.fullVersion} ${appInfo.build}",
+            "JDK" to "${System.getProperty("java.vendor")} ${System.getProperty("java.version")}",
+            "Plugin version" to plugin.version
+    )
+
+    private val detailInfo = mapOf(
+            "Error message" to detailMessage
+    )
+
+    private val additionalInfo = mutableMapOf<String, String>()
+
+    open fun getTitleTags(): Set<String> {
+        return setOf("IntelliJ", "ReportedByUser")
+    }
+
+    open fun getTitle(): String {
+        val tags = getTitleTags()
+                .map { "[$it]" }
+                .reduce { l, r -> "$l$r"}     // Output as: [Tag1][Tag2]
+
+        return "$tags $shortMessage"
+    }
+
+    open fun getBody(): String {
+        return (platformInfo + detailInfo + additionalInfo)
+                .map { "${it.key}: ${it.value}" }
+                .reduce { l, r -> "$l\n$r"}
+    }
+
+    open fun with(key: String, value: String): ReportableError {
+        additionalInfo[key] = value
+
+        return this
+    }
+
+    abstract fun report()
+}
