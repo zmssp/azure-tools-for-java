63eb25846c35ca9b645ee43353bf4fabdd1442c8
Wei Zhang <wezhang@outlook.com>
2018-07-05 22:13:07 -0700

    Prepare a standalone Spark Run configuration submitter for menus
    Signed-off-by: Wei Zhang <wezhang@outlook.com>

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/hdinsight/spark/run/ServerlessSparkRunConfigurationSubmitter.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.microsoft.azure.hdinsight.spark.run
+
+import com.intellij.execution.ExecutorRegistry
+import com.intellij.execution.ProgramRunnerUtil
+import com.intellij.execution.RunManager
+import com.intellij.openapi.project.Project
+import com.microsoft.azure.hdinsight.spark.run.configuration.ServerlessSparkConfigurationFactory
+import com.microsoft.azure.hdinsight.spark.run.configuration.ServerlessSparkConfigurationType
+
+class ServerlessSparkRunConfigurationSubmitter(project: Project, clusterTitle: String) {
+    var runConfiguration = RunManager.getInstance(project).createRunConfiguration(
+            "[Spark Job] to ADL pool $clusterTitle",
+            ServerlessSparkConfigurationFactory(ServerlessSparkConfigurationType()))
+
+    var executor = ExecutorRegistry.getInstance().getExecutorById(SparkBatchJobRunExecutor.EXECUTOR_ID)
+
+    fun submit() {
+        runConfiguration.isEditBeforeRun = true
+
+        ProgramRunnerUtil.executeConfiguration(runConfiguration, executor)
+    }
+}
