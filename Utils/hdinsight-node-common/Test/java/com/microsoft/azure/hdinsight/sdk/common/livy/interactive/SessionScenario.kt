45856ca1e88abd40b4f758c22c7e24bfa1b45e7b
Rui Fang <rufan@microsoft.com>
2018-09-18 16:22:01 +0800

    Replace HttpResponseException with AzureDataLakeException in test code

--- Utils/hdinsight-node-common/Test/java/com/microsoft/azure/hdinsight/sdk/common/livy/interactive/SessionScenario.kt
+++ Utils/hdinsight-node-common/Test/java/com/microsoft/azure/hdinsight/sdk/common/livy/interactive/SessionScenario.kt
@@ -24,12 +24,12 @@ package com.microsoft.azure.hdinsight.sdk.common.livy.interactive
 
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.microsoft.azure.hdinsight.sdk.common.AzureDataLakeException
 import com.microsoft.azure.hdinsight.spark.common.MockHttpService
 import cucumber.api.java.Before
 import cucumber.api.java.en.And
 import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
-import org.apache.http.client.HttpResponseException
 import org.assertj.core.api.Assertions.assertThat
 import java.net.URI
 import kotlin.test.fail
@@ -70,7 +70,7 @@ class SessionScenario {
         }}
     }
 
-    @Then("^check the HttpResponseException\\((\\d+)\\) when creating livy interactive session after creating should be thrown$")
+    @Then("^check the AzureDataLakeException\\((\\d+)\\) when creating livy interactive session after creating should be thrown$")
     fun checkCreateSessionException(statusCodeExpect: Int) {
         sessionMock!!.create()
                 .subscribe(
@@ -78,8 +78,8 @@ class SessionScenario {
                             fail("Get a normal session return without exceptions.")
                         },
                         { err -> run {
-                            assertThat(err).isInstanceOf(HttpResponseException::class.java)
-                            assertThat((err as HttpResponseException).statusCode).isEqualTo(statusCodeExpect)
+                            assertThat(err).isInstanceOf(AzureDataLakeException::class.java)
+                            assertThat((err as AzureDataLakeException).statusCode).isEqualTo(statusCodeExpect)
                         }}
                 )
 
