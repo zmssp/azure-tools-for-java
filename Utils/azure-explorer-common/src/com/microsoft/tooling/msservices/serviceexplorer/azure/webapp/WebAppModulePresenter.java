e7cbcc60e56fdce45e1c6c927ee3b7ec698c7ca9
Summer <victoriasqq@gmail.com>
2018-09-18 13:39:11 +0800

    Revert WebAppModulePresenter onModuleRefresh function (#2028)

--- Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/webapp/WebAppModulePresenter.java
+++ Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/webapp/WebAppModulePresenter.java
@@ -4,36 +4,19 @@ import java.io.IOException;
 
 import com.microsoft.azuretools.core.mvp.model.webapp.AzureWebAppMvpModel;
 import com.microsoft.azuretools.core.mvp.ui.base.MvpPresenter;
-import com.microsoft.tooling.msservices.components.DefaultLoader;
-
-import rx.Observable;
 
 public class WebAppModulePresenter<V extends WebAppModuleView> extends MvpPresenter<V> {
-    private static final String CANNOT_GET_WEB_APPS = "Cannot get web apps.";
     /**
      * Called from view when the view needs refresh.
      */
     public void onModuleRefresh() {
-        Observable.fromCallable(() -> AzureWebAppMvpModel.getInstance().listAllWebApps(true))
-            .subscribeOn(getSchedulerProvider().io())
-            .subscribe(webApps -> DefaultLoader.getIdeHelper().invokeLater(() -> {
-                if (isViewDetached()) {
-                    return;
-                }
-                getMvpView().renderChildren(webApps);
-            }), e -> errorHandler(CANNOT_GET_WEB_APPS, (Exception) e));
+        final WebAppModuleView view = getMvpView();
+        if (view != null) {
+            view.renderChildren(AzureWebAppMvpModel.getInstance().listAllWebApps(true));
+        }
     }
 
     public void onDeleteWebApp(String sid, String id) throws IOException {
         AzureWebAppMvpModel.getInstance().deleteWebApp(sid, id);
-    }
-
-    private void errorHandler(String msg, Exception e) {
-        DefaultLoader.getIdeHelper().invokeLater(() -> {
-            if (isViewDetached()) {
-                return;
-            }
-            getMvpView().onErrorWithException(msg, e);
-        });
     }
 }
