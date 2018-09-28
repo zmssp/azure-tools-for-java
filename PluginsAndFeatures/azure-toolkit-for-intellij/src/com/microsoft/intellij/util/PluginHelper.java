a512d0cb27a2963104bfb96ad3791f23f7c47501
Summer <victoriasqq@gmail.com>
2018-09-28 17:01:00 +0800

    Upgrade Azure SDK to 1.16.0 (#2099)
    * Upgrade Azure SDK to 1.16.0
    
    * Update Azure SDK Version in the properties files
    
    * Update versions in build.gradle and PluginHelper file
    
    * Use json-smart v2.3 to fix CI

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/util/PluginHelper.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/util/PluginHelper.java
@@ -28,7 +28,7 @@ import java.io.File;
 
 public class PluginHelper {
 
-    private static final String AZURE_ARTIFACT = "azure-1.14.0.jar";
+    private static final String AZURE_ARTIFACT = "azure-1.16.0.jar";
 
     /**
      * @return resource filename in plugin's directory
