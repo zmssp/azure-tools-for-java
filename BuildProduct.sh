8552e0416766167e412d25f76f11228a37854d20
Yan Zhang <Eskibear@users.noreply.github.com>
2018-10-31 15:26:43 +0800

    Update production build script (#2325)
    * Update production build script for IntelliJ IDEA 183.
    
    * Missed org.intellij.scala

--- BuildProduct.sh
+++ BuildProduct.sh
@@ -6,16 +6,23 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-
 VERSION="3.14.0"
 MAVEN_QUIET=""
-INTELLIJ_VERSION=false
+INJECT_INTELLIJ_VERSION=false
+
+IJ_VERSION_LATEST=183.3795.13
+IJ_DISPLAY_VERSION_LATEST=2018.3
+IJ_SCALA_VERSION_LATEST=2018.3.2
+
+IJ_VERSION_FALLBACK=2018.2
+IJ_DISPLAY_VERSION_FALLBACK=2018.2
+IJ_SCALA_VERSION_FALLBACK=2018.2.11
 
 while getopts ":hqv" option; do
     case $option in
         h) echo "usage: $0 [-h] [-q] [-v]"; exit ;;
         q) MAVEN_QUIET="-q" ;;
-        v) INTELLIJ_VERSION=true ;;
+        v) INJECT_INTELLIJ_VERSION=true ;;
         ?) echo "error: option -$OPTARG is not implemented"; exit ;;
     esac
 done
@@ -28,7 +35,6 @@ TOSIGNPATH="/c/Signing/ToSign"
 SIGNEDPATH="/c/Signing/Signed"
 ECLIPSE_TOSIGN="/c/jenkins/toSignPackage/eclipse"
 INTELLIJ_TOSIGN="/c/jenkins/toSignPackage/intelliJ"
-CSU_PATH="/c/Signing/CodeSignUtility"
 
 # check dir exists
 if [ ! -d  "$ARTIFACTS_DIR" ]; then
@@ -54,6 +60,7 @@ else
 	exit 1
 fi
 
+# Continue building Utils with signed jars
 cp ${SIGNEDPATH}/azuretools-core-${VERSION}.jar ./Utils/azuretools-core/target/azuretools-core-${VERSION}.jar
 cp ${SIGNEDPATH}/azure-explorer-common-${VERSION}.jar ./Utils/azure-explorer-common/target/azure-explorer-common-${VERSION}.jar
 cp ${SIGNEDPATH}/hdinsight-node-common-${VERSION}.jar ./Utils/hdinsight-node-common/target/hdinsight-node-common-${VERSION}.jar
@@ -64,31 +71,31 @@ mvn install:install-file -Dfile=./Utils/hdinsight-node-common/target/hdinsight-n
 
 mvn install -f ./PluginsAndFeatures/AddLibrary/AzureLibraries/pom.xml -Dmaven.repo.local=./.repository $MAVEN_QUIET
 
-# # Build eclipse plugin
+# Build eclipse plugin
 mvn clean install -f ./PluginsAndFeatures/azure-toolkit-for-eclipse/pom.xml -Dinstrkey=${ECLIPSE_KEY}  $MAVEN_QUIET
 cp ./PluginsAndFeatures/azure-toolkit-for-eclipse/WindowsAzurePlugin4EJ/target/WindowsAzurePlugin4EJ*.zip ./$ARTIFACTS_DIR/WindowsAzurePlugin4EJ.zip
 
 chmod +x ./tools/IntellijVersionHelper
 
-# Build intellij 2018.1 plugin
-if [ $INTELLIJ_VERSION == "true" ] ; then
-    ./tools/IntellijVersionHelper 2018.1
+# Build intellij plugin for latest version
+if [ $INJECT_INTELLIJ_VERSION == "true" ] ; then
+    ./tools/IntellijVersionHelper $IJ_DISPLAY_VERSION_LATEST
 fi
-(cd PluginsAndFeatures/azure-toolkit-for-intellij && ./gradlew clean buildPlugin -s -Papplicationinsights.key=${INTELLIJ_KEY} -Pintellij_version=IC-2018.1 -Pdep_plugins=org.intellij.scala:2018.1.8)
-cp ./PluginsAndFeatures/azure-toolkit-for-intellij/build/distributions/azure-toolkit-for-intellij.zip ./$ARTIFACTS_DIR/azure-toolkit-for-intellij-2018.1.zip
+(cd PluginsAndFeatures/azure-toolkit-for-intellij && ./gradlew clean buildPlugin -s -Papplicationinsights.key=${INTELLIJ_KEY} -Pintellij_version=IC-$IJ_VERSION_LATEST -Pdep_plugins=org.intellij.scala:$IJ_SCALA_VERSION_LATEST)
+cp ./PluginsAndFeatures/azure-toolkit-for-intellij/build/distributions/azure-toolkit-for-intellij.zip ./$ARTIFACTS_DIR/azure-toolkit-for-intellij-$IJ_DISPLAY_VERSION_LATEST.zip
 
-# Build intellij 2018.2 plugin
-if [ $INTELLIJ_VERSION == "true" ] ; then
-    ./tools/IntellijVersionHelper 2018.2
+# Build intellij plugin for fallback version
+if [ $INJECT_INTELLIJ_VERSION == "true" ] ; then
+    ./tools/IntellijVersionHelper $IJ_DISPLAY_VERSION_FALLBACK
 fi
-(cd PluginsAndFeatures/azure-toolkit-for-intellij && ./gradlew clean buildPlugin -s -Papplicationinsights.key=${INTELLIJ_KEY})
-cp ./PluginsAndFeatures/azure-toolkit-for-intellij/build/distributions/azure-toolkit-for-intellij.zip ./$ARTIFACTS_DIR/azure-toolkit-for-intellij-2018.2.zip
+(cd PluginsAndFeatures/azure-toolkit-for-intellij && ./gradlew clean buildPlugin -s -Papplicationinsights.key=${INTELLIJ_KEY} -Pintellij_version=IC-$IJ_VERSION_FALLBACK -Pdep_plugins=org.intellij.scala:$IJ_SCALA_VERSION_FALLBACK)
+cp ./PluginsAndFeatures/azure-toolkit-for-intellij/build/distributions/azure-toolkit-for-intellij.zip ./$ARTIFACTS_DIR/azure-toolkit-for-intellij-$IJ_DISPLAY_VERSION_FALLBACK.zip
 
 # Extract jars to sign
 # intelliJ
 rm -rf ${INTELLIJ_TOSIGN}/*
-unzip -p ./artifacts/azure-toolkit-for-intellij-2018.1.zip azure-toolkit-for-intellij/lib/azure-toolkit-for-intellij.jar > ${INTELLIJ_TOSIGN}/azure-toolkit-for-intellij_2018.1.jar
-unzip -p ./artifacts/azure-toolkit-for-intellij-2018.2.zip azure-toolkit-for-intellij/lib/azure-toolkit-for-intellij.jar > ${INTELLIJ_TOSIGN}/azure-toolkit-for-intellij_2018.2.jar
+unzip -p ./artifacts/azure-toolkit-for-intellij-$IJ_DISPLAY_VERSION_LATEST.zip azure-toolkit-for-intellij/lib/azure-toolkit-for-intellij.jar > ${INTELLIJ_TOSIGN}/azure-toolkit-for-intellij_$IJ_DISPLAY_VERSION_LATEST.jar
+unzip -p ./artifacts/azure-toolkit-for-intellij-$IJ_DISPLAY_VERSION_FALLBACK.zip azure-toolkit-for-intellij/lib/azure-toolkit-for-intellij.jar > ${INTELLIJ_TOSIGN}/azure-toolkit-for-intellij_$IJ_DISPLAY_VERSION_FALLBACK.jar
 
 # Eclipse
 rm -rf ${ECLIPSE_TOSIGN}/*
