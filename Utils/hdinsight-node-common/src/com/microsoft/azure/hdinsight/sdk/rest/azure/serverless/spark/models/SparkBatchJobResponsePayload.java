232d8f0802a00a3ed605e213e32f33e3b002ad22
Rui Fang <rufan@microsoft.com>
2018-11-20 14:56:29 +0800

    modify serverless REST API manually to leverage exisiting code

--- Utils/hdinsight-node-common/src/com/microsoft/azure/hdinsight/sdk/rest/azure/serverless/spark/models/SparkBatchJobResponsePayload.java
+++ Utils/hdinsight-node-common/src/com/microsoft/azure/hdinsight/sdk/rest/azure/serverless/spark/models/SparkBatchJobResponsePayload.java
@@ -23,90 +23,94 @@
 
 package com.microsoft.azure.hdinsight.sdk.rest.azure.serverless.spark.models;
 
-import java.util.Map;
-import java.util.List;
+//import java.util.Map;
+//import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+import com.microsoft.azure.hdinsight.spark.common.SparkSubmitResponse;
 
 /**
+ *  * To leverage Livy(HDInsight) Spark Batch Job codes, some fields are commented
+ *  * since they can be inherited from the base class SparkSubmissionParameter
+ *
  * Spark Batch job information equivalent to livy response payload in OSS.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SparkBatchJobResponsePayload {
-    /**
-     * The session id.
-     */
-    @JsonProperty(value = "id", access = JsonProperty.Access.WRITE_ONLY)
-    private Integer id;
-
-    /**
-     * The application Id of the session.
-     */
-    @JsonProperty(value = "appId", access = JsonProperty.Access.WRITE_ONLY)
-    private String appId;
-
-    /**
-     * The detailed applicationInfo.
-     */
-    @JsonProperty(value = "appInfo", access = JsonProperty.Access.WRITE_ONLY)
-    private Map<String, String> appInfo;
-
-    /**
-     * The log lines.
-     */
-    @JsonProperty(value = "log", access = JsonProperty.Access.WRITE_ONLY)
-    private List<String> log;
-
-    /**
-     * The batch state.
-     */
-    @JsonProperty(value = "state", access = JsonProperty.Access.WRITE_ONLY)
-    private String state;
-
-    /**
-     * Get the session id.
-     *
-     * @return the id value
-     */
-    public Integer id() {
-        return this.id;
-    }
-
-    /**
-     * Get the application Id of the session.
-     *
-     * @return the appId value
-     */
-    public String appId() {
-        return this.appId;
-    }
-
-    /**
-     * Get the detailed applicationInfo.
-     *
-     * @return the appInfo value
-     */
-    public Map<String, String> appInfo() {
-        return this.appInfo;
-    }
-
-    /**
-     * Get the log lines.
-     *
-     * @return the log value
-     */
-    public List<String> log() {
-        return this.log;
-    }
-
-    /**
-     * Get the batch state.
-     *
-     * @return the state value
-     */
-    public String state() {
-        return this.state;
-    }
+public class SparkBatchJobResponsePayload extends SparkSubmitResponse {
+//    /**
+//     * The session id.
+//     */
+//    @JsonProperty(value = "id", access = JsonProperty.Access.WRITE_ONLY)
+//    private Integer id;
+//
+//    /**
+//     * The application Id of the session.
+//     */
+//    @JsonProperty(value = "appId", access = JsonProperty.Access.WRITE_ONLY)
+//    private String appId;
+//
+//    /**
+//     * The detailed applicationInfo.
+//     */
+//    @JsonProperty(value = "appInfo", access = JsonProperty.Access.WRITE_ONLY)
+//    private Map<String, String> appInfo;
+//
+//    /**
+//     * The log lines.
+//     */
+//    @JsonProperty(value = "log", access = JsonProperty.Access.WRITE_ONLY)
+//    private List<String> log;
+//
+//    /**
+//     * The batch state.
+//     */
+//    @JsonProperty(value = "state", access = JsonProperty.Access.WRITE_ONLY)
+//    private String state;
+//
+//    /**
+//     * Get the session id.
+//     *
+//     * @return the id value
+//     */
+//    public Integer id() {
+//        return this.id;
+//    }
+//
+//    /**
+//     * Get the application Id of the session.
+//     *
+//     * @return the appId value
+//     */
+//    public String appId() {
+//        return this.appId;
+//    }
+//
+//    /**
+//     * Get the detailed applicationInfo.
+//     *
+//     * @return the appInfo value
+//     */
+//    public Map<String, String> appInfo() {
+//        return this.appInfo;
+//    }
+//
+//    /**
+//     * Get the log lines.
+//     *
+//     * @return the log value
+//     */
+//    public List<String> log() {
+//        return this.log;
+//    }
+//
+//    /**
+//     * Get the batch state.
+//     *
+//     * @return the state value
+//     */
+//    public String state() {
+//        return this.state;
+//    }
 
 }
