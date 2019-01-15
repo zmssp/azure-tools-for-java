9af7083b6e754d6d9d1ec108b73b0cf26e546708
Kun Huang <cs.huangkun@gmail.com>
2019-01-15 13:18:27 +0800

    Disable interactive console for cosmos serverless run config (#2654)
    * Disable interactive console for cosmos serverless run config

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/hdinsight/spark/console/RunSparkScalaLivyConsoleAction.kt
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/hdinsight/spark/console/RunSparkScalaLivyConsoleAction.kt
@@ -22,12 +22,33 @@
 
 package com.microsoft.azure.hdinsight.spark.console
 
-import com.microsoft.azure.hdinsight.common.logger.ILogger
+import com.intellij.execution.RunManagerEx
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.microsoft.azure.hdinsight.spark.run.configuration.ArisSparkConfiguration
+import com.microsoft.azure.hdinsight.spark.run.configuration.CosmosSparkRunConfiguration
+import com.microsoft.azure.hdinsight.spark.run.configuration.LivySparkBatchJobRunConfiguration
 import org.jetbrains.plugins.scala.console.ScalaConsoleRunConfigurationFactory
 
-class RunSparkScalaLivyConsoleAction : RunSparkScalaConsoleAction(), ILogger {
+class RunSparkScalaLivyConsoleAction : RunSparkScalaConsoleAction() {
     override val consoleRunConfigurationFactory: ScalaConsoleRunConfigurationFactory
         get() = SparkScalaLivyConsoleConfigurationType().confFactory()
 
     override fun getNewSettingName(): String = "Spark Livy Interactive Session Console(Scala)"
+
+    override fun update(event: AnActionEvent) {
+        event.presentation.isEnabled = false
+        super.update(event)
+        val project = CommonDataKeys.PROJECT.getData(event.dataContext) ?: return
+        val selectedConfigSettings = RunManagerEx.getInstanceEx(project).selectedConfiguration
+        var runConfig = selectedConfigSettings?.configuration
+
+        event.presentation.isEnabled = when {
+            runConfig == null -> false
+            runConfig.javaClass == CosmosSparkRunConfiguration::class.java -> true
+            runConfig.javaClass == LivySparkBatchJobRunConfiguration::class.java -> true
+            runConfig.javaClass == ArisSparkConfiguration::class.java -> true
+            else -> false
+        }
+    }
 }
